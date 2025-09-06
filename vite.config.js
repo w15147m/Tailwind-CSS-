c15129556f2b60b1ffbc70e60f795d562fbe440b
waseem abbas <waseemofficee@gamil.com>
2025-09-06 20:45:17 +0500

    update 5 files and delete 1 file

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,18 @@ import {
 } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.jsx' // Changed from app.js to app.jsx for React
+            ],
             refresh: true,
         }),
+        react(), // Added React plugin
         tailwindcss(),
     ],
     server: {
