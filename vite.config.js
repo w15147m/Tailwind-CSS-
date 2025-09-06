25693b3930944f532d74dc25e8e190425f23f4ea
waseem abbas <waseemofficee@gamil.com>
2025-09-06 18:35:27 +0500

    reset code and project

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.jsx', // For your React app
+                'resources/js/alpine.js', // For Alpine.js
+            ],
             refresh: true,
         }),
+        react(),
     ],
 });
